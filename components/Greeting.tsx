c6e301314155c3394538fb9784f5a926c51331fb
pho-veteran <thanhvinhlqt2016@gmail.com>
2025-02-18 14:05:20 +0700

    Inital | GluestackUI, NativeWind

---
+++ components/Greeting.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+
+const Greeting = () => {
+    return (
+        <Text
+            className="text-center text-lg"
+        >
+            Hello, World!
+        </Text>
+    );
+}
+
+export default Greeting;
